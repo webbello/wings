00307bb1849b0c7e6928b68402d73bb57cdc2751
Md Irfan <rashk4irfan@gmail.com>
2019-10-11 13:24:14 +0530

    change team image

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -33,11 +33,11 @@
                 </p>
     
                 <div class="social-links">
-                  <a href="#" class="twitter"><i class="fab fa-twitter"></i></a>
-                  <a href="#" class="facebook"><i class="fab fa-facebook"></i></a>
-                  <a href="#" class="instagram"><i class="fab fa-instagram"></i></a>
-                  <a href="#" class="google-plus"><i class="fab fa-google-plus"></i></a>
-                  <a href="#" class="linkedin"><i class="fab fa-linkedin"></i></a>
+                  <a href="https://twitter.com/wings_of_esc" class="twitter"><i class="fab fa-twitter"></i></a>
+                  <a href="https://www.facebook.com/wingsofescofficial/" class="facebook"><i class="fab fa-facebook"></i></a>
+                  <a href="https://www.instagram.com/wingsofesc_official/" class="instagram"><i class="fab fa-instagram"></i></a>
+                  {{-- <a href="#" class="google-plus"><i class="fab fa-google-plus"></i></a> --}}
+                  <a href="https://www.linkedin.com/company/26616896/" class="linkedin"><i class="fab fa-linkedin"></i></a>
                 </div>
     
               </div>
