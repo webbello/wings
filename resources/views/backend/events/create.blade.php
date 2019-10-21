4a8d43a82bb1805563ff4ca119564c860ec8547a
Md Irfan <irfatechgroup@gmail.com>
2019-10-22 02:05:22 +0530

    Create event in card

--- resources/views/backend/events/create.blade.php
+++ resources/views/backend/events/create.blade.php
@@ -5,71 +5,87 @@
     <link rel="stylesheet" type="text/css" href="/css/bootstrap-datetimepicker.min.css">
 @endpush
 @section('content')
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="float-left">
-                <h2>Add New Events</h2>
-            </div>
-            <div class="float-right">
-                <a class="btn btn-primary" href="{{ route('admin.events.index') }}"> Back</a>
-            </div>
-        </div>
-    </div>
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <strong>Whoops!</strong> There were some problems with your input.<br><br>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-    <form action="{{ route('admin.events.store') }}" method="POST" enctype="multipart/form-data">
-    @csrf
-    <div class="form-group">    
-        <label for="title">Title:</label>
-        <input type="text" class="form-control" name="title"/>
-    </div>
-
-    {{-- <div class="form-group">
-        <label for="summary">Summary:</label>
-        <input type="text" class="form-control" name="summary"/>
-    </div> --}}
-
-    <div class="form-group">
-        <label for="description">Description:</label>
-        <input type="text" class="form-control" name="description"/>
-    </div>
-    <div class="form-group">
-        
-        <label for="event_date">Date:</label>
-        <div class="input-group mb-3 ">
-            <input type="text" class="form-control datetimepicker" placeholder="Event Name" name="event_date" aria-label="Recipient's username" aria-describedby="button-addon2">
-            <div class="input-group-append datetimepicker">
-                <button class="btn btn-outline-secondary" type="button" id="button-addon2"><i class="fas fa-calendar-alt"></i></button>
-            </div>
-        </div>
-        {{-- <input type="text" class="form-control datetimepicker" name="event_date"/> --}}
-    </div>
-    <div class="form-group">
-        <label for="venue">Venue:</label>
-        <input type="text" class="form-control" name="venue"/>
-    </div>
-    <div class="form-group">
-        <label for="city">City:</label>
-        <input type="text" class="form-control" name="city"/>
-    </div>
-    <div class="form-group">
-        <label for="country">Country:</label>
-        <input type="text" class="form-control" name="country"/>
-    </div>
-    <div class="form-group">
-        <label for="image">Image:</label>
-        <input type="file" class="form-control" name="image"/>
-    </div>                         
-    <button type="submit" class="btn btn-primary">Add event</button>
-    </form>
+    <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-sm-5">
+                        <h4 class="card-title mb-0">
+                            @lang('Add New Event')
+                            <small class="text-muted">@lang('Upcoming')</small>
+                        </h4>
+                    </div><!--col-->
+                    <div class="col-sm-7">
+                            <div class="float-right">
+                                <a class="btn btn-primary" href="{{ route('admin.events.index') }}"> Back</a>
+                            </div>
+                    </div><!--col-->
+                </div><!--row-->
+    
+                <hr>
+    
+                <div class="row mt-4">
+                    <div class="col">
+                        <form action="{{ route('admin.events.store') }}" method="POST" enctype="multipart/form-data">
+                            @csrf
+                            <div class="form-group">    
+                                <label for="title">Title:</label>
+                                <input type="text" class="form-control" name="title"/>
+                            </div>
+                        
+                            {{-- <div class="form-group">
+                                <label for="summary">Summary:</label>
+                                <input type="text" class="form-control" name="summary"/>
+                            </div> --}}
+                        
+                            <div class="form-group">
+                                <label for="description">Description:</label>
+                                <input type="text" class="form-control" name="description"/>
+                            </div>
+                            <div class="form-group">
+                                
+                                <label for="event_date">Date:</label>
+                                <div class="input-group mb-3 ">
+                                    <input type="text" class="form-control datetimepicker" placeholder="Event Name" name="event_date" aria-label="Recipient's username" aria-describedby="button-addon2">
+                                    <div class="input-group-append datetimepicker">
+                                        <button class="btn btn-outline-secondary" type="button" id="button-addon2"><i class="fas fa-calendar-alt"></i></button>
+                                    </div>
+                                </div>
+                                {{-- <input type="text" class="form-control datetimepicker" name="event_date"/> --}}
+                            </div>
+                            <div class="form-group">
+                                <label for="venue">Venue:</label>
+                                <input type="text" class="form-control" name="venue"/>
+                            </div>
+                            <div class="form-group">
+                                <label for="city">City:</label>
+                                <input type="text" class="form-control" name="city"/>
+                            </div>
+                            <div class="form-group">
+                                <label for="country">Country:</label>
+                                <input type="text" class="form-control" name="country"/>
+                            </div>
+                            <div class="form-group">
+                                <label for="image">Image:</label>
+                                <input type="file" class="form-control" name="image"/>
+                            </div>                         
+                            <button type="submit" class="btn btn-primary">Add event</button>
+                        </form>
+                    </div><!--col-->
+                </div><!--row-->
+            </div><!--card-body-->
+    
+            <div class="card-footer">
+                <div class="row">
+                    <div class="col">
+                        {{-- {{ form_cancel(route('admin.auth.role.index'), __('buttons.general.cancel')) }} --}}
+                    </div><!--col-->
+    
+                    <div class="col text-right">
+                        {{-- {{ form_submit(__('buttons.general.crud.create')) }} --}}
+                    </div><!--col-->
+                </div><!--row-->
+            </div><!--card-footer-->
+        </div><!--card-->
 @endsection
 @push('after-scripts')
     <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment-with-locales.js"></script>
