8105886999b8309ff5ca6e7a58e6e1b3b17af1fd
Md Irfan <mohd.irfan@evontech.com>
2019-11-20 11:02:29 +0530

    Bug-fix and design tweaks

--- resources/views/backend/gallery/index.blade.php
+++ resources/views/backend/gallery/index.blade.php
@@ -1,49 +1,73 @@
 @extends('backend.layouts.app')
 
-@section('title', app_name() . ' | '. __('labels.backend.access.roles.management'))
+@section('title', app_name() . ' | ' . __('labels.backend.access.users.management'))
+
+@section('breadcrumb-links')
+    @include('backend.auth.user.includes.breadcrumb-links')
+@endsection
 
 @section('content')
-<div class="row">
-    <div class="col-lg-12 margin-tb">
-        
-        <div class="float-right">
-            <a class="btn btn-success" href="{{ route('admin.gallery.create') }}"> Create New photo</a>
-        </div>
-    </div>
-</div>
-@if ($message = Session::get('success'))
-    <div class="alert alert-success">
-        <p>{{ $message }}</p>
-    </div>
-@endif
-<table class="table table-bordered">
-    <tr>
-        <th>No</th>
-        <th>Image</th>
-        <th>Album</th>
-        <th>Name</th>
-        <th>Details</th>
-        <th width="280px">Action</th>
-    </tr>
-    @foreach ($photos as $photo)
-    <tr>
-        <td>{{ ++$i }}</td>
-        <td><a href="/storage/uploads/album/{{ $photo->album_id }}/{{ $photo->image }}"><img src="/storage/uploads/album/{{ $photo->album_id }}/{{ $photo->image }}" alt="" width="60px"> </a> </td>
-        <td>{{$photo->album->title}}</td>
-        <td>{{ $photo->title }}</td>
-        <td>{{ $photo->description }}</td>
-        <td>
-            <form action="{{ route('admin.gallery.destroy',$photo->id) }}" method="POST">
-                <a class="btn btn-info" href="{{ route('admin.gallery.show',$photo->id) }}">Show</a>
-                <a class="btn btn-primary" href="{{ route('admin.gallery.edit',$photo->id) }}">Edit</a>
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="btn btn-danger">Delete</button>
-            </form>
-        </td>
-    </tr>
-    @endforeach
-</table>
+<div class="card">
+    <div class="card-body">
+        <div class="row">
+            <div class="col-sm-5">
+                <h4 class="card-title mb-0">
+                    {{ __('Gallery') }} <small class="text-muted">{{ __('Photos') }}</small>
+                </h4>
+            </div><!--col-->
 
-{!! $photos->links() !!}
+            <div class="col-sm-7">
+                @include('backend.gallery.includes.header-buttons')
+            </div><!--col-->
+        </div><!--row-->
+
+        <div class="row mt-4">
+            <div class="col">
+                <div class="table-responsive">
+                    <table class="table table-bordered">
+                        <tr>
+                            <th>No</th>
+                            <th>Image</th>
+                            <th>Album</th>
+                            <th>Name</th>
+                            <th>Details</th>
+                            <th width="280px">Action</th>
+                        </tr>
+                        @foreach ($photos as $photo)
+                        <tr>
+                            <td>{{ ++$i }}</td>
+                            <td><a href="/storage/uploads/album/{{ $photo->album_id }}/{{ $photo->image }}"><img src="/storage/uploads/album/{{ $photo->album_id }}/{{ $photo->image }}" alt="" width="60px"> </a> </td>
+                            <td>{{$photo->album->title}}</td>
+                            <td>{{ $photo->title }}</td>
+                            <td>{{ $photo->description }}</td>
+                            <td>
+                                <form action="{{ route('admin.gallery.destroy',$photo->id) }}" method="POST">
+                                    <a class="btn btn-info" href="{{ route('admin.gallery.show',$photo->id) }}">Show</a>
+                                    <a class="btn btn-primary" href="{{ route('admin.gallery.edit',$photo->id) }}">Edit</a>
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </table>
+                </div>
+            </div><!--col-->
+        </div><!--row-->
+        <div class="row">
+            <div class="col-7">
+                <div class="float-left">
+                    {{-- {!! $users->total() !!} {{ trans_choice('labels.backend.access.users.table.total', $users->total()) }} --}}
+                </div>
+            </div><!--col-->
+
+            <div class="col-5">
+                <div class="float-right">
+                    {!! $photos->links() !!}
+                </div>
+            </div><!--col-->
+        </div><!--row-->
+    </div><!--card-body-->
+</div><!--card-->
 @endsection
