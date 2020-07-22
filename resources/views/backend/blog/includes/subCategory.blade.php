13e76a96ae3ff5ea3cfb6defdcd89d0f49e9e47b
Mohammed Irfan <mohammed.irfan@walkme.com>
2020-07-22 10:28:10 +0530

    typofix Blog into blog

--- resources/views/backend/blog/includes/subCategory.blade.php
+++ resources/views/backend/blog/includes/subCategory.blade.php
@@ -1,6 +1,6 @@
 @foreach($subcategories as $subcategory)
         <option value="{{$subcategory->id}}">|--{{$subcategory->name}}</option>
 	    @if(count($subcategory->subcategory))
-            @include('backend.Blog.includes.subCategory',['subcategories' => $subcategory->subcategory])
+            @include('backend.blog.includes.subCategory',['subcategories' => $subcategory->subcategory])
         @endif
 @endforeach
--- resources/views/backend/blog/includes/subCategoryEdit.blade.php
+++ resources/views/backend/blog/includes/subCategoryEdit.blade.php
@@ -1,6 +1,6 @@
 @foreach($subcategories as $subcategory)
         <option value="{{$subcategory->id}}" {{($subcategory->id == $post->category_id) ? 'selected' : ''}}>|--{{$subcategory->name}}</option>
 	    @if(count($subcategory->subcategory))
-            @include('backend.Blog.includes.subCategoryEdit',['subcategories' => $subcategory->subcategory])
+            @include('backend.blog.includes.subCategoryEdit',['subcategories' => $subcategory->subcategory])
         @endif
 @endforeach
