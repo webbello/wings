d15e2c3981769bf9d2ae5ceaf094d823e75f8912
Mohammed <mohammed.irfan@walkme.com>
2020-08-31 20:26:38 +0530

    fix nan issue in counter

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -37,10 +37,10 @@
             var distance = countDownDate - now;
                 
             // Time calculations for days, hours, minutes and seconds
-            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24)) || '00';
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) || '00';
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)) || '00';
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000) || '00';
                 
             // Output the result in an element with id="demo"
             document.getElementById("seconds").innerHTML = seconds;
